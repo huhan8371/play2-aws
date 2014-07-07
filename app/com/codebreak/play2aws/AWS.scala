1a7ab99d5375b8ea6491624df616f802d8fc1856
Naoki Takezoe <naoki.takezoe@bizreach.co.jp>
2014-07-07 14:59:43 +0900

    Add entry point to AWS API. The current implementation supports only CloudSearch.

---
+++ app/com/codebreak/play2aws/AWS.scala
@@ -0,0 +1,18 @@
+package com.codebreak.play2aws
+
+/**
+ * Provides entry point to API for AWS services.
+ */
+object AWS {
+
+  /**
+   * Returns the CloudSearch client.
+   */
+  def CloudSearch() = com.codebreak.cloudsearch4s.CloudSearch(
+    com.codebreak.cloudsearch4s.CloudSearch.CloudSearchSettings(
+      registerUrl = Config.get("cloudsearch.register.url").get,
+      searchUrl   = Config.get("cloudsearch.search.url").get
+    )
+  )
+
+}
